2785ad7b59e716709b467577e5cc10d78d12f6a5
la5chine <khachinechaker@gmail.com>
2023-07-23 16:18:56 +0100

    whenever the user types in the search box we hit the marvel api to extract the results and set them using setResults passed as a prop

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 import { TextField, Theme } from '@lumx/react';
 import { mdiMagnify } from '@lumx/icons';
 
-const Search = () => (
-	<TextField theme={Theme.dark} placeholder="Search ..." icon={mdiMagnify} onChange={() => {}} />
-);
+const Search = ({ setResults }) => {
+
+
+  const fetchUserData = (text) => {
+    const params = new URLSearchParams({apikey: '8fb61ed0d8ae4cc76943c8d2b8e2ed9c', nameStartsWith: text})
+    const endpoint = 'https://gateway.marvel.com:443/v1/public/characters?' + params
+
+    fetch(endpoint)
+      .then(response => {
+        return response.json();
+      })
+      .then(({ data }) => {
+        setResults(data?.results ? data.results : []);
+      });
+  };
+  return (
+    <TextField theme={Theme.dark} placeholder="Search ..."
+               icon={mdiMagnify} onChange={fetchUserData}/>
+  );
+};
 
 export default Search;
