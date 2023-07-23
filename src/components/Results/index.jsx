a0c012dbbeadd93f9903fc4a38c4862ab8f5e4c1
la5chine <khachinechaker@gmail.com>
2023-07-23 16:54:31 +0100

    move characters cards list into a new component called CharacterSection to keep pagination separate

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
 import { AspectRatio, List } from '@lumx/react';
 import { ListItem, Size, Thumbnail } from '@lumx/react';
+import CharacterSection from '../CharacterSection';
 
 
 const Results = ({ results }) => {
 
 
   return (
-    <List>
-      {
-        results.map((result, index) => (
-          <ListItem
-            key={index}
-            size={Size.big}
-          >
-            <Thumbnail
-              image={result.thumbnail.path + '.' + result.thumbnail.extension}
-              alt="Marvel-character"
-              aspectRatio={AspectRatio.original}
-              size={Size.xl}
-            />
-            <span>{result.name}</span>
-          </ListItem>
-        ))
-      }
-    </List>
+    <CharacterSection results={results}/>
   );
 };
 
