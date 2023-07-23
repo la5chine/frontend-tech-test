8e7a68f00d9d8397c5b614eba2dfbf79c312485e
la5chine <khachinechaker@gmail.com>
2023-07-23 16:26:07 +0100

    use existing api helper function get to fetch data from endpoint

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { TextField, Theme } from '@lumx/react';
 import { mdiMagnify } from '@lumx/icons';
+import { get } from '../../api';
 
 const Search = ({ setResults }) => {
 
 
   const fetchUserData = (text) => {
-    const params = new URLSearchParams({apikey: '8fb61ed0d8ae4cc76943c8d2b8e2ed9c', nameStartsWith: text})
-    const endpoint = 'https://gateway.marvel.com:443/v1/public/characters?' + params
-
-    fetch(endpoint)
-      .then(response => {
-        return response.json();
-      })
+    get("characters", {nameStartsWith: text})
       .then(({ data }) => {
-        setResults(data?.results ? data.results : []);
-      });
+        setResults(data.data?.results ? data.data.results : []);
+      })
   };
   return (
     <TextField theme={Theme.dark} placeholder="Search ..."
