5ee123e6ca4a379c945c632d018254a0251b9482
la5chine <khachinechaker@gmail.com>
2023-07-23 16:12:45 +0100

    create a Result component to list all the marvel characters in a list, expects a results prop to be passed containing the list characters

---
+++ src/components/Results/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AspectRatio, List } from '@lumx/react';
+import { ListItem, Size, Thumbnail } from '@lumx/react';
+
+
+const Results = ({ results }) => {
+
+
+  return (< List>
+    {
+      results.map((result, index) => (
+        <ListItem
+          key={index}
+          size={Size.big}
+        >
+          <Thumbnail
+            image={result.thumbnail.path + '.' + result.thumbnail.extension}
+            alt="Marvel-character"
+            aspectRatio={AspectRatio.original}
+            size={Size.xl}
+          />
+          <span>{result.name}</span>
+        </ListItem>
+      ))
+    }
+  </List>);
+};
+
+
+export default Results;
