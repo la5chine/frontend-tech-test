41f7639b6fa8f4d1f483704f907c9a1a63260abd
la5chine <khachinechaker@gmail.com>
2023-07-23 16:14:15 +0100

    create a MainPage component to hold both the Header and the Results components

---
+++ src/components/MainPage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header';
+import Results from '../Results';
+
+
+const MainPage = () => {
+  return (
+    <>
+      <Header/>
+      <Results/>
+    </>
+  );
+};
+
+export default MainPage;
