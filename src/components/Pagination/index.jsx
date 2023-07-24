e0cf2649192ee7443cd0ccb1ec4ea35ccc1522cf
la5chine <khachinechaker@gmail.com>
2023-07-24 15:30:57 +0100

    shoe pagination buttons dynamically depending on the total number of items and paginate when clicking on any page

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -10,15 +10,34 @@ const Pagination = ({
   const postsPerPage = 4;
   const NumberOfPages = Math.ceil(totalCount / postsPerPage);
 
+  const getShownPages = (selectedPage) => {
+    const dummyShownPages = [1, 2, parseInt(selectedPage), parseInt(selectedPage) + 1, parseInt(selectedPage) + 2,
+      NumberOfPages - 1, NumberOfPages];
+    return dummyShownPages.filter(
+      (pageNumber, index) => {
+        return (pageNumber < NumberOfPages + 1 && pageNumber >= 1 && dummyShownPages.indexOf(pageNumber) === index);
+      }
+    )
+      .sort(function (a, b) {
+        return a - b;
+      });
+  };
+
+  const [shownPages, setShownPages] = useState(getShownPages(1));
+
+  const onPageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    setShownPages(getShownPages(pageNumber));
+    return paginate(pageNumber);
+  };
+
 
   return (
     <>
-      {[...Array(NumberOfPages + 1)
-        .keys()].slice(1)
-        .map((pageNumber) =>
-          <Button value={pageNumber} onClick={(event) => paginate(event.currentTarget.value)}
-                  key={pageNumber} isDisabled={currentPage === pageNumber}>{pageNumber}</Button>
-        )}
+      {shownPages.map((pageNumber, index) =>
+        <Button value={pageNumber} onClick={(event) => onPageChange(event.currentTarget.value)}
+                key={index} isDisabled={currentPage == pageNumber}>{pageNumber}</Button>
+      )}
     </>
   );
 };
