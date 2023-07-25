a229ce950d6f24467f9e58904a9d39644116abca
la5chine <khachinechaker@gmail.com>
2023-07-25 13:20:24 +0100

    set whether the search input is valid or has errors (meaning there is no results)

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Theme } from '@lumx/react';
 import { mdiMagnify } from '@lumx/icons';
 import { searchCharacters } from '../../api';
 
 const Search = ({ setResults, setTotalCount, setSearchTerm }) => {
-
+  const [isValid, setIsValid] = useState(true)
+  const [hasError, setHasError] = useState(false)
 
   const fetchUserData = (searchTerm) => {
     setSearchTerm(searchTerm);
     if (searchTerm === '') {
+      setHasError(false)
+      setIsValid(true)
       setTotalCount(0)
       setResults([])
       return
@@ -17,12 +20,19 @@ const Search = ({ setResults, setTotalCount, setSearchTerm }) => {
       .then(({ data }) => {
         setTotalCount(data.data?.total ? data.data.total : 0);
         setResults(data.data?.results ? data.data.results : []);
+
+        setHasError(data.data?.total === 0);
+        setIsValid(data.data?.total > 0);
       });
 
   };
   return (
     <TextField theme={Theme.dark} placeholder="Search ..."
-               icon={mdiMagnify} onChange={fetchUserData}/>
+               icon={mdiMagnify} onChange={fetchUserData}
+               clearButtonProps={{ label: 'Icon'}}
+               isValid={isValid}
+               hasError={hasError}
+    />
   );
 };
 
