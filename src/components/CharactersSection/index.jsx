61104402960d2ca114aa17974e0e0cb78948f795
la5chine <khachinechaker@gmail.com>
2023-07-25 12:58:38 +0100

    add style to the result of the search

--- src/components/CharactersSection/index.jsx
+++ src/components/CharactersSection/index.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Chip } from '@lumx/react';
 
 
 const CharactersSection = ({ results }) => {
 
 
   return (
-    <div>
+    <div className="result-container">
       {
         results.map((result, index) => (
-          <div key={index}>
+          <div key={index} className="result-card">
             <img
               src={result.thumbnail.path + '.' + result.thumbnail.extension}
               alt="Marvel-character"
               className={"item-image"}
             />
-            <span>{result.name}</span>
+            <div className="result-card-info">
+              <h1>{result.name}</h1>
+              <span>{result.description}</span>
+              <div>
+                <Chip>#comics: {result.comics.available}</Chip>
+                <Chip>#series: {result.series.available}</Chip>
+                <Chip>#stories: {result.stories.available}</Chip>
+              </div>
+            </div>
           </div>
         ))
       }
